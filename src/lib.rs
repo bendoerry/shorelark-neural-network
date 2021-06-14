d2b3ef228935db5c640a41f0e42fb4e0d3744701
Benjamin Doerry <ben.doerry@gmail.com>
2021-06-14 17:51:55 +0100

    Use map for creating layers from windows

--- src/lib.rs
+++ src/lib.rs
@@ -27,18 +27,12 @@ impl Network {
     pub fn random(layers: Vec<LayerTopology>) -> Self {
         assert!(layers.len() > 1);
 
-        let mut built_layers = Vec::new();
+        let layers = layers
+            .windows(2)
+            .map(|layers| Layer::random(layers[0].neurons, layers[1].neurons))
+            .collect();
 
-        for adjacent_layers in layers.windows(2) {
-            let input_neurons = adjacent_layers[0].neurons;
-            let output_neurons = adjacent_layers[1].neurons;
-
-            built_layers.push(Layer::random(input_neurons, output_neurons));
-        }
-
-        Self {
-            layers: built_layers,
-        }
+        Self { layers }
     }
 }
 
